227d15be3fdc144696f56f805fcaf4356f0f2158
Sidwick <wesley.s.daniels@gmail.com>
2018-02-17 20:15:30 -0600

    Update server.js

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ io.on('connection', function(client) {
 	    });
 
     client.on('messages', function(data) {
-	//client.emit('broad', data);
-	client.broadcast.emit('broad',data);
+	client.emit('broad', data);
+	//client.broadcast.emit('broad',data);
     })
 
 })
