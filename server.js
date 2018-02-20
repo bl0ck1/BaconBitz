6ae4e23abbb31a131c0a87acb78d264022eff5c7
bl0ck1 <36583269+bl0ck1@users.noreply.github.com>
2018-02-20 12:21:09 -0600

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ io.on('connection', function(client) {
 	    });
 
     client.on('messages', function(data) {
-	 console.log(date)
+	 console.log(data)
 	//client.emit('broad', data);
 	client.broadcast.emit('broad',data);
     })
